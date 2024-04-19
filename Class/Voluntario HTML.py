bcc9dd628688218c5cbf39cf5fd9ab571b81370b
VenturaIgnacioo <145007269+VenturaIgnacioo@users.noreply.github.com>
2024-04-19 08:03:58 +0200

    Create Voluntario HTML.py

---
+++ Class/Voluntario HTML.py
@@ -0,0 +1,95 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>SounHood</title>
+    <link href="./index.css" rel="stylesheet">
+    <link rel="stylesheet" href="Normalize.css">
+</head>
+
+<body>
+    <div class="hero">
+        <nav class="container-nav">
+            <img src="/Images/foto png.png" class="logo-principal">
+
+
+            <ul class="nav-ul-first">
+                <li><a href="#"> Shop </a></li>
+                <li><a href="#"> Contact </a></li>
+                <li><a href="#"> services </a></li>
+                <li><a href="#"> Experience </a></li>
+            </ul>
+        </nav>
+    </div>
+
+    <section class="container-all">
+        <img src="/Imagenes-init/Proyecto nuevo.jpg" alt="edicion" class="img-init">
+        <div class="first-view">
+            <span class="blur"></span>
+            <span class="blur"></span>
+            <h2 class="first-title">Welcome to <spam>Sounhood</spam>
+            </h2>
+            <button class="boton-continuar" name="continuar" type="button">Continuar</button>
+        </div>
+    </section>
+
+    <article class="container-service">
+        <div class="first-boxservice">
+            <img src="/Images/IMG 1.jpeg" class="img_box1">
+            <div class="first_box_text">
+                <h2>
+                    <spam>INSTRUMENTOS</spam>
+                </h2>
+                <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Animi debitis repellat esse
+                    necessitatibus
+                    consequuntur voluptatibus voluptatum. Minus dolores earum aut soluta, assumenda optio omnis
+                    incidunt
+                    voluptas non nisi culpa. Quos.</p>
+
+                <a href="#" target="_blank" class="vermas_box">Join Now -></a>
+            </div>
+        </div>
+
+        <div class="second-boxservice">
+            <img src="/Images/IMG 1.jpeg" class="img_box1">
+            <div class="first_box_text">
+                <h2>
+                    <spam>Cuerdas</spam>
+                </h2>
+                <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Animi debitis repellat esse
+                    necessitatibus
+                    consequuntur voluptatibus voluptatum. Minus dolores earum aut soluta, assumenda optio omnis
+                    incidunt
+                    voluptas non nisi culpa. Quos.</p>
+                <a href="#" target="_blank" class="vermas_box">Join Now -></a>
+            </div>
+        </div>
+
+        <div class="first-boxservice">
+            <img src="/Images/IMG 1.jpeg" class="img_box1">
+            <div class="first_box_text">
+                <h2>
+                    <spam>Cubiculos</spam>
+                </h2>
+                <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Animi debitis repellat esse
+                    necessitatibus
+                    consequuntur voluptatibus voluptatum. Minus dolores earum aut soluta, assumenda optio omnis
+                    incidunt
+                    voluptas non nisi culpa. Quos.</p>
+                <a href="#" target="_blank" class="vermas_box">Join Now -></a>
+            </div>
+
+        </div>
+    </article>
+
+
+    <footer>
+
+
+    </footer>
+</body>
+
+</html>
